44370b772f25873aa9b0de57cf12f5221ec05552
taro-arumakan <taro.arumakan@gmail.com>
2024-02-28 12:03:33 +0900

    format document. align to text-align

--- confirm.php
+++ confirm.php
@@ -2,140 +2,208 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+
 <head>
 
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width" />
 
-<title>コモエンカサ</title>
+  <title>コモエンカサ</title>
 
-<!-- SEO -->
-<meta name="description" content="コモエンカサ　表参道駅徒歩2分にあるプレスクール託児所。お母様とお子様に優しい環境をご提供しています。" />
-<meta name="keywords" content="託児所,プレスクール,預かり,一時預かり,駅近,表参道,渋谷,神宮,子供,レッスン,お子様,港区" />
-<!-- close.SEO -->
+  <!-- SEO -->
+  <meta name="description" content="コモエンカサ　表参道駅徒歩2分にあるプレスクール託児所。お母様とお子様に優しい環境をご提供しています。" />
+  <meta name="keywords" content="託児所,プレスクール,預かり,一時預かり,駅近,表参道,渋谷,神宮,子供,レッスン,お子様,港区" />
+  <!-- close.SEO -->
 
-<!-- links -->
-<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-<link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
-<link rel="stylesheet" href="css/style.css" type="text/css" />
-<link rel="stylesheet" href="css/sp.css" type="text/css" />
-<script src="js/modernizr-2.6.2.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/jquery-1.8.2.min.js"><\/script>')</script>
-<script src="js/common.js"></script>
+  <!-- links -->
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <link rel="stylesheet" href="css/sp.css" type="text/css" />
+  <script src="js/modernizr-2.6.2.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script>
+    window.jQuery || document.write('<script src="js/jquery-1.8.2.min.js"><\/script>')
+  </script>
+  <script src="js/common.js"></script>
 
-<!-- close.links -->
-<!--[if lte IE 6]>
+  <!-- close.links -->
+  <!--[if lte IE 6]>
 <script type="text/javascript" src="js/DD_belatedPNG_0.0.8a-min.js"></script>
 <script type="text/javascript">
 DD_belatedPNG.fix('body *');
 </script>
 <![endif]-->
 
-<!-- OGP -->
-<meta property="og:title" content="コモエンカサ">
-<meta property="og:type" content="website">
-<meta property="og:description" content="コモエンカサ　表参道駅徒歩2分にあるプレスクール託児所。お母様とお子様に優しい環境をご提供しています。">
-<meta property="og:url" content="http://www.comoencasa.jp/">
-<meta property="og:image" content="icon">
-<meta property="og:site_name" content="COMO EN CASA">
-<meta property="og:email" content="info@comoencasa.jp">
-<!-- close.OGP -->
+  <!-- OGP -->
+  <meta property="og:title" content="コモエンカサ">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="コモエンカサ　表参道駅徒歩2分にあるプレスクール託児所。お母様とお子様に優しい環境をご提供しています。">
+  <meta property="og:url" content="http://www.comoencasa.jp/">
+  <meta property="og:image" content="icon">
+  <meta property="og:site_name" content="COMO EN CASA">
+  <meta property="og:email" content="info@comoencasa.jp">
+  <!-- close.OGP -->
 
 </head>
 
 <style>
-#menu_fixed.conf{display:block;top:0;}
-#thanks{padding:50px 0 0;}
-#confirm p{text-align: center;font-size:14px;padding:50px 0 0;}
-#form_inf{width:100%;font-size:14px;font-weight:bold;}
-#form_inf td,#form_inf th{padding:10px;word-break:break-all;}
-#form_inf th{width:120px;}
-#confirm .bg_gr{width:600px;margin:0 auto;padding:40px;}
-#confirm input{border: 0px solid #FFF; padding: 10px 40px 10px 40px; background: #0A6450; color: #FFF; font-size: 14px;cursor: pointer;margin-top:30px;width: 180px;}
-#confirm input:hover{background: #53ac5a;cursor: pointer;}
+  #menu_fixed.conf {
+    display: block;
+    top: 0;
+  }
+
+  #thanks {
+    padding: 50px 0 0;
+  }
+
+  #confirm p {
+    text-align: center;
+    font-size: 14px;
+    padding: 50px 0 0;
+  }
+
+  #form_inf {
+    width: 100%;
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  #form_inf td,
+  #form_inf th {
+    padding: 10px;
+    word-break: break-all;
+  }
+
+  #form_inf th {
+    width: 120px;
+  }
+
+  #confirm .bg_gr {
+    width: 600px;
+    margin: 0 auto;
+    padding: 40px;
+  }
+
+  #confirm input {
+    border: 0px solid #FFF;
+    padding: 10px 40px 10px 40px;
+    background: #0A6450;
+    color: #FFF;
+    font-size: 14px;
+    cursor: pointer;
+    margin-top: 30px;
+    width: 180px;
+  }
+
+  #confirm input:hover {
+    background: #53ac5a;
+    cursor: pointer;
+  }
 
 
-@media screen and (max-width: 680px){
-#menu_fixed.conf{display:none;}
-#thanks{padding: 0;}
-#confirm .bg_gr{width:90%;padding:5%;}
-#form_inf th{width:90px;}
-#form_inf td,#form_inf th{font-size:12px;}
-#confirm input{width: 40%;padding:2%;}
-}
+  @media screen and (max-width: 680px) {
+    #menu_fixed.conf {
+      display: none;
+    }
+
+    #thanks {
+      padding: 0;
+    }
+
+    #confirm .bg_gr {
+      width: 90%;
+      padding: 5%;
+    }
+
+    #form_inf th {
+      width: 90px;
+    }
+
+    #form_inf td,
+    #form_inf th {
+      font-size: 12px;
+    }
+
+    #confirm input {
+      width: 40%;
+      padding: 2%;
+    }
+  }
 </style>
 
 
 <body>
 
-<!-- /////////////  ヘッダー /////////////// -->
+  <!-- /////////////  ヘッダー /////////////// -->
 
-<div id="menu_fixed" class="conf">
+  <div id="menu_fixed" class="conf">
     <div class="menu">
-        <nav>
-            <ul class="cf">
-                <li class="home"><a href="index.html"><img src="img/home.png"></a></li>
-                <li><a href="index.html#concept"><img src="img/concept.png"><br/>コンセプト<span>Concept</span></a></li>
-                <li><a href="index.html#schedule"><img src="img/schedule.png"><br/>スケジュール<span>Schedule</span></a></li>
-                <li><a href="index.html#price"><img src="img/price.png"><br/>ご利用料金<span>Price</span></a></li>
-                <li><a href="index.html#class"><img src="img/class.png"><br/>クラス<span>Class</span></a></li>
-                <li><a href="index.html#ccclub"><img src="img/ccclub.png"><br/>コモエンカサクラブ<span>Como en casa club</span></a></li>
-                <li><a href="index.html#teacher"><img src="img/teacher.png"><br/>先生紹介<span>Teacher</span></a></li>
-                <li class="sub">
-                    <ul>
-                        <li><a href="index.html#recruitment">採用情報</a></li>
-                        <li><a href="index.html#rental">レンタルスペース</a></li>
-                        <li><a href="index.html#contact">お問い合わせ</a></li>
-                    </ul>
-                </li>
+      <nav>
+        <ul class="cf">
+          <li class="home"><a href="index.html"><img src="img/home.png"></a></li>
+          <li><a href="index.html#concept"><img src="img/concept.png"><br />コンセプト<span>Concept</span></a></li>
+          <li><a href="index.html#schedule"><img src="img/schedule.png"><br />スケジュール<span>Schedule</span></a></li>
+          <li><a href="index.html#price"><img src="img/price.png"><br />ご利用料金<span>Price</span></a></li>
+          <li><a href="index.html#class"><img src="img/class.png"><br />クラス<span>Class</span></a></li>
+          <li><a href="index.html#ccclub"><img src="img/ccclub.png"><br />コモエンカサクラブ<span>Como en casa club</span></a></li>
+          <li><a href="index.html#teacher"><img src="img/teacher.png"><br />先生紹介<span>Teacher</span></a></li>
+          <li class="sub">
+            <ul>
+              <li><a href="index.html#recruitment">採用情報</a></li>
+              <li><a href="index.html#rental">レンタルスペース</a></li>
+              <li><a href="index.html#contact">お問い合わせ</a></li>
             </ul>
-        </nav>
+          </li>
+        </ul>
+      </nav>
     </div>
     <span class="bg_bottom"></span>
-</div><!-- menu_fixed -->
+  </div><!-- menu_fixed -->
 
 
-<!-- /////////////  完了画面 /////////////// -->
-<section>
+  <!-- /////////////  完了画面 /////////////// -->
+  <section>
     <div id="thanks" class="content">
 
 
-        <div id="confirm">
+      <div id="confirm">
 
-			<!--FORM START-->
-			<?php if($_SESSION['sendflg'] != 1) { ?>
-			<?php echo $explain; ?>
+        <!--FORM START-->
+        <?php if ($_SESSION['sendflg'] != 1) { ?>
+          <?php echo $explain; ?>
 
-                <form action="<?php echo $filename; ?>" method="post">
-                    <div class="cf bg_gr">
-                        </span><span class="bg_left"></span><span class="bg_right"></span><span class="bg_bottom"></span>
+          <form action="<?php echo $filename; ?>" method="post">
+            <div class="cf bg_gr">
+              </span><span class="bg_left"></span><span class="bg_right"></span><span class="bg_bottom"></span>
 
-                    <table id="form_inf">
-                        <span class="bg_top">
+              <table id="form_inf">
+                <span class="bg_top">
 
-                    	<?php confirm(); ?>
+                  <?php confirm(); ?>
 
-                        <tr>
-                        <td colspan="2" align="center">
-                                <?php submit(); ?>
-                        </td>
-                        </tr>
-                    </table>
-                    </div>
-                </form>
+                  <tr>
+                    <td colspan="2" text-align="center">
+                      <?php submit(); ?>
+                    </td>
+                  </tr>
+              </table>
+            </div>
+          </form>
 
 
-			<?php } else if($_SESSION['sendflg'] == 1) transmited(); ?>
-			<!--FORM END-->
+        <?php } else if ($_SESSION['sendflg'] == 1) transmited(); ?>
+        <!--FORM END-->
 
-        </div>
+      </div>
 
-</section><!-- concept -->
+  </section><!-- concept -->
 
-<!-- foot -->
+  <!-- foot -->
 
 
 </body>
+
 </html>
